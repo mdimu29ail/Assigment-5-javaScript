98292b3ca95a9e7262512d20909ba77e7dec87ae
MD Imamul <mdimu29@gmai.com>
2025-03-01 11:05:05 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+document.getElementById("new-date").textContent =
+  new Date().toLocaleDateString();
+
+document.getElementById("clear-log").addEventListener("click", function () {
+  document.getElementById("activity-log").innerHTML = "";
+});
+
+document.getElementById("discover-box").addEventListener("click", function () {
+  console.log("hi");
+  window.location.href = "./main.html";
+});
+function changeBackgroundColor() {
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
+}
+
+document
+  .getElementById("task-container")
+  .addEventListener("click", function (event) {
+    let button = event.target;
+    if (!button.classList.contains("completed-btn") || button.disabled) return;
+
+    alert("Task marked as completed!");
+    button.disabled = true;
+    button.classList.replace("bg-blue-500", "bg-gray-400");
+
+    document.getElementById("task-count").textContent--;
+    document.getElementById("completed-count").textContent++;
+
+    let taskTitle = button
+      .closest(".space-y-3")
+      .querySelector("h2").textContent;
+    taskTitle = taskTitle === "Fix Mobile Button Issue" ? "null" : taskTitle;
+
+    document.getElementById(
+      "activity-log"
+    ).innerHTML += `<p class="text-md font-semibold">You have completed the task: ${taskTitle} at ${new Date().toLocaleTimeString()}</p>`;
+  });
